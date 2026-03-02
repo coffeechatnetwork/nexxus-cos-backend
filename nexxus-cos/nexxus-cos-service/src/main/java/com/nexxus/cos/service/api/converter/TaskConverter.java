2c82b59bd4646faddf98ac360e0d39158feb719f
jun-nexxus <jiang.jun@nexxus.io>
2026-03-02 21:28:34 +0800

    feat: fix style.

--- nexxus-cos/nexxus-cos-service/src/main/java/com/nexxus/cos/service/api/converter/TaskConverter.java
+++ nexxus-cos/nexxus-cos-service/src/main/java/com/nexxus/cos/service/api/converter/TaskConverter.java
@@ -12,7 +12,6 @@ import com.nexxus.cos.service.service.UserService;
 import com.nexxxus.file.api.FileApi;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
