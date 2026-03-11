f3525f16d80c7b35f937c2aa2fe75313c859c5f0
jun-nexxus <jiang.jun@nexxus.io>
2026-03-11 14:45:24 +0800

    feat: auth api refactor.

--- nexxus-server/src/main/java/com/nexxus/server/config/PublicUriProperties.java
+++ nexxus-server/src/main/java/com/nexxus/server/config/PublicUriProperties.java
@@ -8,9 +8,9 @@ public class PublicUriProperties {
     private static final AntPathMatcher PATH_MATCHER = new AntPathMatcher();
     private static final String[] PUBLIC_URI_PATTERNS = new String[]{
             "/actuator/**",
-            "/v1/auth/**",
-            "/v1/apps/*/register",
-            "/v1/apps/*/login",
+            "/api/v1/auth/**",
+            "/api/v1/apps/*/register",
+            "/api/v1/apps/*/login",
             "/swagger-ui/**",
             "/swagger-ui.html",
             "/api-docs/**",
