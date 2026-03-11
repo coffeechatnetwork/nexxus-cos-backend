42978e85c7250fd551925863eb03ede3b6806927
jun-nexxus <jiang.jun@nexxus.io>
2026-03-11 14:49:59 +0800

    feat: api refactor.

--- nexxus-server/src/main/java/com/nexxus/server/controller/v1/UserController.java
+++ nexxus-server/src/main/java/com/nexxus/server/controller/v1/UserController.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 
 @Slf4j
 @RestController
-@RequestMapping("/v1/users")
+@RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserApi userApi;
